18dbc98e640986c393f2f99bd5dbc78c26909f05
MisaSM <misaelsarabiamolina@gmail.com>
2023-10-23 11:58:39 -0700

    Progress made, new controller created

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticaCrud_MisaelSarabia.Data;
+using PracticaCrud_MisaelSarabia.Models;
+
+namespace PracticaCrud_MisaelSarabia.Controllers
+{
+    public class ContactoController : Controller
+    {
+        ContactoData contactoData = new ContactoData();
+        public IActionResult GetAll()
+        {
+            var lista = contactoData.GetContacts();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Save() {
+             
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Save(ContactoModel model) 
+        {
+            var respuesta = contactoData.SaveContact(model);
+            if (respuesta)
+            {
+                return RedirectToAction("GetAll");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
